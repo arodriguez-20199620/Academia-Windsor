aefef235a7e37cafcb7dbce7f2ecd6c91d17cfe7
arodriguez-20199620 <arodriguez-2019620@kinal.edu.gt>
2024-08-18 20:08:34 -0600

    Configuracion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from 'primereact/button';
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+
+      <Button label="Submit" className='bg-pink-400 border-none outline-none ring-0' />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
