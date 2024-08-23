f10832d0727d6aab8e96a307fc7d82bf069579a6
arodriguez-20199620 <arodriguez-2019620@kinal.edu.gt>
2024-08-22 18:01:56 -0600

    cambio de ruta de las imagnes

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,17 +1,17 @@
 import nosotros1 from './nosotros1.jpg';
 import nosotros2 from './nosotros2.jpg';
-import alemania from 'src/assets/img/icons/alemania.png'; // Ruta absoluta
-import espanol from './icons/español.png';
-import francia from './icons/francia.png';
-import italiano from './icons/italiano.png';
-import portugues from './icons/Portugués.png';
-import reinoUnido from './icons/reinoUnido.png';
+import alemania from '/alemania.png'; // Ruta absoluta
+import espanol from './español.png';
+import francia from './francia.png';
+import italiano from './italiano.png';
+import portugues from './Portugués.png';
+import reinoUnido from './reinoUnido.png';
 import corporate from './corporate.jpg'
 import tutoring from './tutoring.jpg'
 import persona1 from './persona1.jpg'
 import persona2 from './persona2.jpg'
 import persona3 from './persona3.jpg'
-import logo from './icons/logo.png';
+import logo from './logo.png';
 
 export {
     logo,
